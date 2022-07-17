4c7b06405798c8c887e9be832048cfd97bfc4824
psi <cy.lonsido@gmail.com>
2022-07-17 23:46:42 +0800

    register pinia

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
 import 'modules/normalize.css' // load reset prior to app styles
 import App from './App.vue'
+import pinia from './stores'
 import router from './router'
 
 createApp(App)
+.use(pinia)
 .use(router)
 .mount('#app')
