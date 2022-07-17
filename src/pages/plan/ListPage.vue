e8278dcd86210f9a259bcbf959233cf2a99a4068
psi <cy.lonsido@gmail.com>
2022-07-17 23:47:05 +0800

    render sample data

--- src/pages/plan/ListPage.vue
+++ src/pages/plan/ListPage.vue
@@ -1,9 +1,39 @@
 <script setup lang='ts'>
-const message = ref('Plan List Page')
+import { storeToRefs } from 'pinia';
+import usePlanStore from '../../stores/plan';
+import { data } from '../../stores/sampleData'
+
+const $plan = storeToRefs(usePlanStore())
+
+onBeforeMount(() => {
+   usePlanStore().TestGetPlans(data.plans)
+})
 </script>
 
 <template>
    <div>
-      {{ message }}
+      <ul>
+         <li v-for='plan in usePlanStore().plans' :key='plan.id'>
+            <div>{{ plan.id }}</div>
+            <div>{{ plan.name }}</div>
+            <div>{{ plan.description }}</div>
+            <div>{{ plan.type }}</div>
+            <div>{{ plan.status }}</div>
+            <ul>
+               <li v-for='child in plan.children' :key='child.id'>
+                  <div>{{ child.id }}</div>
+                  <div>{{ child.name }}</div>
+                  <div>{{ child.type }}</div>
+                  <ul v-if='child.children'>
+                     <li v-for='grandchild in child.children'>
+                        <div>{{ grandchild.id }}</div>
+                        <div>{{ grandchild.name }}</div>
+                        <div>{{ grandchild.type }}</div>
+                     </li>
+                  </ul>
+               </li>
+            </ul>
+         </li>
+      </ul>
    </div>
 </template>
