a882504c3ca5e637aaa06858307520525408a417
psi <cy.lonsido@gmail.com>
2022-07-17 23:55:37 +0800

    resolve pinia error on test

--- src/pages/plan/__test__/ListPage.spec.ts
+++ src/pages/plan/__test__/ListPage.spec.ts
@@ -1,13 +1,15 @@
 import { mount } from '@vue/test-utils'
 import { mergeWith } from 'lodash'
-import { TestMountingOptions } from '../../../test'
+import { TestMountingOptions } from '../../../utils/test-hooks'
+import { createTestingPinia } from '@pinia/testing'
 import ListPage from '../ListPage.vue'
 
+const pinia = createTestingPinia()
 
 const createWrapper = (overrides?: TestMountingOptions) => {
    const defaultMountingOptions: TestMountingOptions = {
       global: {
-         //
+         plugins: [pinia]
       }
    }
    return mount(ListPage as any, mergeWith(defaultMountingOptions, overrides))
