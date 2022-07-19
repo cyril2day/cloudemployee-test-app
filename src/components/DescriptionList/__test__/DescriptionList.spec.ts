c6cf4cb4dbfc8f82b5a4df6253d98dfe9b9941e6
psi <cy.lonsido@gmail.com>
2022-07-19 23:35:40 +0800

    update description list component test

--- src/components/DescriptionList/__test__/DescriptionList.spec.ts
+++ src/components/DescriptionList/__test__/DescriptionList.spec.ts
@@ -1,5 +1,69 @@
+import { mount } from "@vue/test-utils"
+import { mergeWith } from "lodash"
+import { TestMountingOptions } from "../../../utils/test-hooks"
+import DescriptionListVue from "../DescriptionList.vue"
+
+
+const data = [{ _id: '123abc', name: 'Plan 1', description: 'Sample description only', type: 'Plan', status: 'Active' }]
+
+const createWrapper = (overrides?: TestMountingOptions) => {
+   const defaultMountingOptions: TestMountingOptions = {
+      global: {
+         //
+      },
+      props: {
+         data: data
+      } 
+   }
+
+   return mount(DescriptionListVue, mergeWith(defaultMountingOptions, overrides))
+}
+
+
 describe('Description List Component Unit Test', () => {
-   test('sanity check', () => {
-      return
+   beforeEach(() => {
+      vi.resetModules()
+   })
+
+   test(`has single props called 'data' that accepts an array`, () => {
+      expect.assertions(4)
+      const wrapper = createWrapper()
+      const propName = 'data'
+
+      expect(wrapper.props(propName)).toHaveLength(1)
+      expect(wrapper.props(propName)).toBeTypeOf('object')
+      expect(wrapper.props(propName)).toBeInstanceOf(Array)
+      expect(wrapper.props('invalidPropName')).toBe(undefined)
+   })
+
+   test('contains list items as slots when passed a data', () => {
+      expect.assertions(2)
+      const localData = data.concat(
+         { _id: '456def', name: 'Plan 2', description: 'This is the second', type: 'Plan', status: 'Active' }
+      )
+      const wrapper = createWrapper({
+         props: {
+            data: localData
+         } 
+      })
+
+      expect(wrapper.findAll('dl').length).toBe(2)
+      expect(wrapper.find('dl').element.classList.contains('list__wrapper')).toBe(true)
+   })
+
+   test('has status icon present on the page', () => {
+      expect.assertions(1)
+      const wrapper = createWrapper()
+      
+      expect(wrapper.find('i').element.classList.contains('cib-discover')).toBe(true)
+   })
+})
+
+describe('Description List Component Snapshot Test', () => {
+   test('Elements and styles renders correctly', () => {
+      expect.assertions(1)
+      const wrapper = createWrapper()
+
+      expect(wrapper.element).toMatchSnapshot()
    })
 })
