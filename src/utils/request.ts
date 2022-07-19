bdd0c6b9f4187860680664dc7339dbec3ecdb34c
psi <cy.lonsido@gmail.com>
2022-07-19 12:51:19 +0800

    set backend baseuri

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 
-const api = axios.create({ baseURL: 'http://localhost:8000' })
+const api = axios.create({ baseURL: 'https://cloudemployee-backend-test-app.vercel.app/api' })
 
 api.interceptors.request.use(
    config => {
