08a08261f2244763121c064e71ab500053192cc1
psi <cy.lonsido@gmail.com>
2022-07-21 11:09:16 +0800

    add navigation and add-child buttons

--- src/components/DescriptionList/DescriptionList.vue
+++ src/components/DescriptionList/DescriptionList.vue
@@ -12,7 +12,6 @@ const props = defineProps({
    }
 })
 
-
 async function editPlan(id: string) {
    console.log(id)
 }
@@ -51,8 +50,19 @@ function getNumOfChild(item: Plan) {
             </div>
       </div>
       <div class='list__actions'>
-         <button class='list__edit' @click='editPlan(item._id)'><i class="cil-pencil"></i></button>
-         <button class='list__delete' @click='removePlan(item._id)'><i class="cil-trash"></i></button>
+         <div class='list__actions--rest'>
+            <button class='list__add-child'><i class="cil-plus"></i></button>
+            <button class='list__edit' @click='editPlan(item._id)'><i class="cil-pencil"></i></button>
+            <button class='list__delete' @click='removePlan(item._id)'><i class="cil-trash"></i></button>
+         </div>
+         <div class='list__actions--navigate'>
+            <button
+               v-if="getNumOfChild(item)"
+               class='list__forward'
+            >
+               <i class="cil-caret-right"></i>
+            </button>
+         </div>
       </div>
    </dl>
 </template>
