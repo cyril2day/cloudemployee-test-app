bed6402f70ef847f9c2ff46c3243ee50b8321aab
psi <cy.lonsido@gmail.com>
2022-07-22 13:49:14 +0800

    update dashboard

--- src/pages/DashboardPage.vue
+++ src/pages/DashboardPage.vue
@@ -1,19 +1,18 @@
 <script setup lang='ts'>
-
+const src = 'https://continuity2.com'
 </script>
 
 <template>
-   <div>
-      Dashboard Page
-   </div>
+   <iframe :src='src' />
 </template>
 
 <style lang="scss" scoped>
-div {
+iframe {
    display: grid;
    grid-template-columns: repeat(12, 1fr);
    grid-template-rows: repeat(10, 1fr);
-   height: 90vh;
+   height: 95vh;
+   width: 100%;
    gap: 2px;
    background: var(--background-white);
 } 
